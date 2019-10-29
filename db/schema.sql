f721c4f0503bd191dfa23d20e8e6a7493d140b6a
SirHexxus <jamesmichaelstacy@gmail.com>
2019-10-29 16:37:11 -0700

    set up database schema

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+-- Clear out any existing database that matches the name, so that we don't have overlap --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create database --
+CREATE DATABASE burgers_db;
+
+-- Switch to or use the `burgers_db` --
+USE burgers_db;
+
+-- Create a `burgers` table with these fields:
+CREATE TABLE burgers (
+    -- id: an auto incrementing int --
+    id INT AUTO_INCREMENT NOT NULL,
+    -- burger_name: a string --
+    burger_name VARCHAR(255) NOT NULL,
+    -- devoured: a boolean --
+    devoured BOOLEAN DEFAULT false,
+    -- Set "id" as Primary Key --
+    PRIMARY KEY(id)
+);
