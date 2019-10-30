b93e6729e1a3d7e04a942e5242fdb1e05df28fed
SirHexxus <jamesmichaelstacy@gmail.com>
2019-10-29 22:24:51 -0700

    setup routes and router in controller'

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,41 @@
+//  
+const express = require('express');
+const router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+const burger = require('../models/burger.js');
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+  burger.all((data) => {
+    const hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.insert({
+        name: req.body.name,
+        isEaten: req.body.isEaten
+    }, (result) => {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:name", function(req, res) {
+    cat.update(req.params.name, req.body.isEaten, (result) => {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
