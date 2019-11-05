5fbec0eb3cdc9c367111f4593ff9aa9b9cc60bd4
SirHexxus <jamesmichaelstacy@gmail.com>
2019-11-04 18:42:55 -0800

    changed schema to match JawsDB database info

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
 -- Clear out any existing database that matches the name, so that we don't have overlap --
-DROP DATABASE IF EXISTS burgers_db;
+DROP DATABASE IF EXISTS y5c0lsa5ara5euml;
 
 -- Create database --
-CREATE DATABASE burgers_db;
+CREATE DATABASE y5c0lsa5ara5euml;
 
--- Switch to or use the `burgers_db` --
-USE burgers_db;
+-- Switch to or use the `y5c0lsa5ara5euml` --
+USE y5c0lsa5ara5euml;
 
 -- Create a `burgers` table with these fields:
 CREATE TABLE burgers (
