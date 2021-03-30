c3b8d4d464f542bccde0dbb15e6c1e9081e64ee2
SirHexxus <jamesmichaelstacy@gmail.com>
2021-03-30 11:12:04 -0700

    changed sql files from burgers to customers

---
+++ db/customer_seeds.sql
@@ -0,0 +1,37 @@
+INSERT INTO customers
+VALUES (
+        1,
+        "JMS Web Designs",
+        true,
+        "28740 Paseo Diablo",
+        "PO Box #2468, Lake Arrowhead, CA 92352",
+        "James Stacy",
+        74.99
+    ),
+    (
+        2,
+        "Examples-R-Us",
+        true,
+        "123 Example Ln.",
+        "123 Example Ln.",
+        "Roberta Exampleton",
+        85.00
+    ),
+    (
+        3,
+        "Lobster Place",
+        false,
+        "321 Undahdahsee Rd.",
+        "55 Dontbillme Ave.",
+        "Jordan Peterson",
+        51.50
+    ),
+    (
+        4,
+        "Evil League of Evil",
+        false,
+        "2112 Evil Blvd.",
+        "Secret Lair, Hwy 666",
+        "Dr. Emmet Vil",
+        1000.99
+    );
--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,34 @@
 -- Switch to or use the `y5c0lsa5ara5euml` --
 USE y5c0lsa5ara5euml;
-
-DROP TABLE IF EXISTS burgers;
-
--- Create a `burgers` table with these fields:
-CREATE TABLE burgers (
+-- DROP TABLE IF EXISTS burgers;
+-- -- Create a `burgers` table with these fields:
+-- CREATE TABLE burgers (
+--     -- id: an auto incrementing int --
+--     id INT AUTO_INCREMENT NOT NULL,
+--     -- burger_name: a string --
+--     burger_name VARCHAR(255) NOT NULL,
+--     -- devoured: a boolean --
+--     devoured BOOLEAN NOT NULL DEFAULT false,
+--     -- Set "id" as Primary Key --
+--     PRIMARY KEY(id)
+-- );
+DROP TABLE IF EXISTS customers;
+-- Create a `customers` table with these columns:
+CREATE TABLE customers (
     -- id: an auto incrementing int --
     id INT AUTO_INCREMENT NOT NULL,
-    -- burger_name: a string --
-    burger_name VARCHAR(255) NOT NULL,
-    -- devoured: a boolean --
-    devoured BOOLEAN NOT NULL DEFAULT false,
+    -- customer_name: a string --
+    customer_name VARCHAR(255) NOT NULL,
+    -- active_customer: a boolean --
+    active_customer BOOLEAN NOT NULL DEFAULT false,
+    -- business_address: a string --
+    business_address VARCHAR(255) NOT NULL,
+    -- billing_address: a string --
+    billing_address VARCHAR(255),
+    -- contact_name: a string --
+    contact_name VARCHAR(255) NOT NULL,
+    -- service_rate: a decimal of up to 6 digits, with two places past 0
+    service_rate DECIMAL(6, 2) NOT NULL,
     -- Set "id" as Primary Key --
     PRIMARY KEY(id)
 );
